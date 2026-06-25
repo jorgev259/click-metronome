8fc5c992405c42d34f76f933595519ab8622467e
Jorge Vargas <jorge.vargascorrales@wnco.com>
2026-06-25 12:01:11 -0600

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/click-metronome',
+  base: '/click-metronome',
   plugins: [react(), tailwindcss()]
 })
