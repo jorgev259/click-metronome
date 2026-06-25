ee1c77c1ba64f026f15bfb57ce33fdac88df76d6
Jorge Vargas <jorgev259@outlook.es>
2026-06-24 23:56:37 -0600

    Agregar reflections of sound

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { HashRouter, Route, Routes } from 'react-router'
 import Layout from './components/layout.tsx'
 import { malaguenaSteps } from './songs/malaguena.tsx'
 import { presagioSteps } from './songs/presagio.ts'
+import { reflectionsSoundSong } from './songs/reflections-sound.ts'
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
@@ -18,6 +19,10 @@ if (rootElement) {
           <Route element={<Layout />}>
             <Route path='malaguena' element={<App song={malaguenaSteps} />} />
             <Route path='presagio' element={<App song={presagioSteps} />} />
+            <Route
+              path='reflections-sound'
+              element={<App song={reflectionsSoundSong} />}
+            />
           </Route>
         </Routes>
       </HashRouter>
---
+++ src/songs/reflections-sound.ts
@@ -0,0 +1,18 @@
+import type { Step } from '../lib/types'
+
+export const reflectionsSoundSong: Step[] = [
+  { measure: 5, bpm: 80, compasses: 2 },
+  { compasses: 4 },
+  { compasses: 2 },
+  { label: 'Sax Solo', compasses: 2 },
+  { measure: 4, compasses: 1 },
+  { label: ' ', compasses: 4 },
+  { label: 'Echo Effect', measure: 5, compasses: 3 },
+  { measure: 4, compasses: 6 },
+  { measure: 5, compasses: 2 },
+  { compasses: 4 },
+  { bpm: 90, compasses: 4 },
+  { measure: 4, compasses: 1 },
+  { bpm: 72, compasses: 1 },
+  { bpm: 80, measure: 5, compasses: 3 }
+]
